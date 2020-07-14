1aa0bc438eab34cc127f43c527e47b58b877a9ea
edmundchan <pokedev8@gmail.com>
2020-07-14 14:17:56 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace intern_ally.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
